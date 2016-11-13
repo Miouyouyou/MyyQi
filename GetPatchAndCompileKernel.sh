b33ed578a29f4765609b3973f34c4d5cbe4b7bc2
Myy <myy@miouyouyou.fr>
2016-11-13 11:19:11 +0000

    Documented the build procedure
    Signed-off-by: Myy <myy@miouyouyou.fr>

--- GetPatchAndCompileKernel.sh
+++ GetPatchAndCompileKernel.sh
@@ -1,7 +1,3 @@
-This is a working patched 4.9-rc4 kernel, using the torvalds branch as a basis.
-
-The kernel was compiled using the following procedure :
-```bash
 # Get the kernel
 git clone --depth 1 --branch v4.9-rc4 'git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git' &&
 cd linux
@@ -48,37 +44,4 @@ make rk3288-miqi.dtb zImage modules -j5
 # mkdir /tmp/MyyQi/boot &&
 # make INSTALL_MOD_PATH=/tmp/MyyQi modules_install &&
 # make INSTALL_PATH=/tmp/MyyQi/boot install
-```
-
-This procedure was stored in the GetPatchAndCompileKernel.sh file and can be run like this :
-```bash
-sh GetPatchAndCompileKernel.sh
-```
-
-You will need compiling tools, **git**, **wget** and **find** in order to execute this procedure successfully.
-
-The patches applied are stored in the **patches/** folder.
-
-To install this kernel, copy the **zImage** and the **rk3288-miqi.dtb** file in your boot partition.
-Note that if you have access to U-boot through a serial console AND your MiQi is powered through your USB computer, you can access the whole eMMC like a USB memory stick using the following command :
-```
-ums 0 mmc 1
-```
-
-TODO
-----
-
-- [x] Document how to use the generated kernel and boot it
-- [x] Add the [Open Source Kernel-space Mali Midgard drivers](http://malideveloper.arm.com/resources/drivers/open-source-mali-midgard-gpu-kernel-drivers/)
-- [ ] Add [gator](https://github.com/ARM-software/gator)
-- [ ] Document how to use [DS-5 : Streamline](https://developer.arm.com/products/software-development-tools/ds-5-development-studio/streamline/overview) to analyse OpenGL ES 2.x/3.x programs running on MiQi boards using such kernels.
-
-Tipping
--------
-
-[Pledgie !](https://pledgie.com/campaigns/32702)
-
-BTC: 16zwQUkG29D49G6C7pzch18HjfJqMXFNrW
-
-[![Tip with Altcoins](https://shapeshift.io/images/shifty/small_light_altcoins.png)](https://shapeshift.io/shifty.html?destination=16zwQUkG29D49G6C7pzch18HjfJqMXFNrW&output=BTC)
 
