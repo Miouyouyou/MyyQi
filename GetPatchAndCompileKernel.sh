fee144f49b14e8fbed89134f32860e21f00685c8
Myy <myy@miouyouyou.fr>
2017-01-10 08:25:08 +0000

    Fixed the new Compilation script and its copy in the README.
    Signed-off-by: Myy <myy@miouyouyou.fr>

--- GetPatchAndCompileKernel.sh
+++ GetPatchAndCompileKernel.sh
@@ -12,6 +12,16 @@ function download_and_apply_patches {
 }
 
 export KERNEL_VERSION=v4.10-rc3
+export MALI_VERSION=r15p0-00rel0
+
+export GITHUB_REPO=Miouyouyou/MyyQi
+export GIT_BRANCH=master
+
+export BASE_FILES_URL=https://raw.githubusercontent.com
+export PATCHES_FOLDER_URL=$BASE_FILES_URL/$GITHUB_REPO/$GIT_BRANCH/patches
+export KERNEL_PATCHES_FOLDER_URL=$PATCHES_FOLDER_URL/kernel/$KERNEL_VERSION
+export MALI_PATCHES_FOLDER=$PATCHES_FOLDER_URL/Mali/$MALI_VERSION
+
 git clone --depth 1 --branch $KERNEL_VERSION 'git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git' &&
 cd linux
 
@@ -19,9 +29,9 @@ export SRC_DIR=$PWD
 
 # Download, prepare and copy the Mali Kernel-Space drivers. 
 # Some TGZ are AWFULLY packaged with everything having 0777 rights.
-export MALI_VERSION=r15p0-00rel0
+
 wget "http://malideveloper.arm.com/downloads/drivers/TX011/$MALI_VERSION/TX011-SW-99002-$MALI_VERSION.tgz" &&
-tar zxvf $PATCHES_DIR/TX011-SW-99002-$MALI_VERSION.tgz &&
+tar zxvf TX011-SW-99002-$MALI_VERSION.tgz &&
 cd TX011-SW-99002-$MALI_VERSION &&
 find . -type 'f' -exec chmod 0644 {} ';' &&
 find . -type 'd' -exec chmod 0755 {} ';' &&
@@ -38,10 +48,7 @@ rm -r TX011-SW-99002-$MALI_VERSION TX011-SW-99002-$MALI_VERSION.tgz
 # Apply the Rockchip DRM, Rockchip fbdev, RK3288 DTS and
 # Kconfig/Makefile patches used to enable the compilation of the
 # Mali driver
-export GITHUB_REPO=Miouyouyou/MyyQi
-export GIT_BRANCH=master
-export PATCHES_FOLDER_URL=https://raw.githubusercontent.com/$GITHUB_REPO/$GIT_BRANCH/patches
-export KERNEL_PATCHES_FOLDER_URL=$PATCHES_FOLDER_URL/kernel/$KERNEL_VERSION
+
 #
 ## TODO : The following patterns should be rewritten as a function...
 export PATCHES="
@@ -61,17 +68,15 @@ export PATCHES="
 0002-UMP-Adapt-get_user_pages-calls.patch
 0003-Renamed-Kernel-DMA-Fence-structures-and-functions.patch
 "
-export MALI_PATCHES_FOLDER=$PATCHES_FOLDER_URL/Mali/$MALI_VERSION
+
 download_and_apply_patches $MALI_PATCHES_FOLDER $PATCHES
 unset PATCHES
 
 # Get the configuration file and compile the kernel
-git apply -v $PATCHES_DIR/patches/Mali/$MALI_VERSION/*
-git apply -v $PATCHES_DIR/patches/kernel/$KERNEL_VERSION/*
 export ARCH=arm
 export CROSS_COMPILE=armv7a-hardfloat-linux-gnueabi-
 make mrproper
-wget -O .config "https://raw.githubusercontent.com/$GITHUB_REPO/$GIT_BRANCH/boot/config-4.10.0-rc3RockMyyX-rc+"
+wget -O .config "$BASE_FILES_URL/$GITHUB_REPO/$GIT_BRANCH/boot/config-4.10.0-rc3RockMyyX-rc+"
 make rk3288-miqi.dtb zImage modules -j5
 
 # Kernel compiled
