efe2146020e445788b234a7776f5ddd93498525b
Myy <myy@miouyouyou.fr>
2017-01-16 15:44:09 +0000

    New v4.9 branch, including Willy Tarreau DTS modifications
    Willy Tarreau generated a lot of patches when trying to get the best
    possible performances from RK3288 MiQi devices, using 4.9 kernels.
    You can have a look at his work here :
    https://forum.mqmaker.com/t/miqi-based-build-farm-finally-up-and-running/605
    
    Also included the Unified Memory Provider and activated DMA fences in
    the Mali driver. Thanks to this, performances with DRM and Wayland are
    now top-notch !
    
    Signed-off-by: Myy <myy@miouyouyou.fr>

--- GetPatchAndCompileKernel.sh
+++ GetPatchAndCompileKernel.sh
@@ -1,51 +1,79 @@
-# Get the kernel
+function download_and_apply_patches {
+	base_url=$1
+	patches=${@:2}
+	for patch in $patches; do
+		wget $base_url/$patch || 
+		{ echo "Could not download $patch"; exit 1; }
+	done
+	git apply $patches
+	rm $patches
+}
+
 export KERNEL_VERSION=v4.9
-#git clone --depth 1 --branch $KERNEL_VERSION 'git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git' &&
-#cd linux
+export MALI_VERSION=r15p0-00rel0
+
+export GITHUB_REPO=Miouyouyou/MyyQi
+export GIT_BRANCH=v4.9
+
+export BASE_FILES_URL=https://raw.githubusercontent.com
+export PATCHES_FOLDER_URL=$BASE_FILES_URL/$GITHUB_REPO/$GIT_BRANCH/patches
+export KERNEL_PATCHES_FOLDER_URL=$PATCHES_FOLDER_URL/kernel/$KERNEL_VERSION
+export MALI_PATCHES_FOLDER=$PATCHES_FOLDER_URL/Mali/$MALI_VERSION
+
+export MALI_PATCHES="
+0001-Adapt-get_user_pages-calls-to-use-the-new-calling-pr.patch
+0002-Adapt-the-UMP-code-to-new-calls.patch
+"
+export KERNEL_PATCHES="
+0001-Rockchip-DRM-and-Framebuffer-patches-from-ARM-softwa.patch
+0002-Integrate-the-Mali-GPU-address-to-the-rk3288-and-rk3.patch
+0003-Post-Mali-Kernel-device-drivers-modifications.patch
+0004-Add-the-Mali-Unified-Memory-Provider-to-the-kernel.patch
+0005-ARM-dts-rockchip-fix-the-regulator-s-voltage-range-o.patch
+0006-ARM-dts-rockchip-fix-the-MiQi-board-s-LED-definition.patch
+0007-ARM-dts-rockchip-add-the-MiQi-board-s-fan-definition.patch
+0008-ARM-dts-rockchip-add-support-for-1800-MHz-operation-.patch
+0009-clk-rockchip-add-all-known-operating-points-to-the-a.patch
+0010-ARM-dts-rockchip-miqi-add-turbo-mode-operating-point.patch
+"
+
+# Get the kernel
+git clone --depth 1 --branch $KERNEL_VERSION 'git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git' &&
+cd linux
+
+export SRC_DIR=$PWD
 
 # Download, prepare and copy the Mali Kernel-Space drivers. 
 # Some TGZ are AWFULLY packaged with everything having 0777 rights.
-export MALI_VERSION=r15p0-00rel0
+
 wget "http://malideveloper.arm.com/downloads/drivers/TX011/$MALI_VERSION/TX011-SW-99002-$MALI_VERSION.tgz" &&
 tar zxvf TX011-SW-99002-$MALI_VERSION.tgz &&
 cd TX011-SW-99002-$MALI_VERSION &&
-find . -type 'f' -exec chmod 0644 {} ';' &&
-find . -type 'd' -exec chmod 0755 {} ';' &&
-find . -name 'sconscript' -exec rm {} ';' &&
-rm -r 'driver/product/kernel/patches' 'driver/product/kernel/license.txt' &&
-cp -r driver/product/kernel/drivers/gpu/arm ../drivers/gpu/ &&
-cp -r driver/product/kernel/drivers/base/ump ../drivers/base/ &&
-cd .. &&
+find . -type 'f' -exec chmod 0644 {} ';' && # Every file   should have -rw-r--r-- rights
+find . -type 'd' -exec chmod 0755 {} ';' && # Every folder should have drwxr-xr-x rights
+find . -name 'sconscript' -exec rm {} ';' && # Remove sconscript files. Useless.
+cd driver/product/kernel &&
+rm -r 'patches' 'license.txt' && # Remove the patches and GPL license file.
+cp -r drivers/gpu/arm  $SRC_DIR/drivers/gpu/ && # Copy the Midgard code
+cp -r drivers/base/ump $SRC_DIR/drivers/base/ && # Copy the Unified Memory Provider code
+cp include/linux/ump*  $SRC_DIR/include/linux/ && # Copy the Unified Memory Provider headers.
+cp include/linux/kds.h $SRC_DIR/include/linux/ && # Copy the Kernel Dependency System header ↑ (dependency)
+cd $SRC_DIR &&
 rm -r TX011-SW-99002-$MALI_VERSION TX011-SW-99002-$MALI_VERSION.tgz
 
 # Apply the Rockchip DRM, Rockchip fbdev, RK3288 DTS and
 # Kconfig/Makefile patches used to enable the compilation of the
 # Mali driver
-export GITHUB_REPO=Miouyouyou/MyyQi
-export GIT_BRANCH=master
-export PATCHES_FOLDER_URL=https://raw.githubusercontent.com/$GITHUB_REPO/$GIT_BRANCH/patches
-export KERNEL_PATCHES_FOLDER_URL=$PATCHES_FOLDER_URL/kernel/$KERNEL_VERSION
-wget $KERNEL_PATCHES_FOLDER_URL/0001-Rockchip-DRM-and-Framebuffer-patches-from-ARM-softwa.patch &&
-wget $KERNEL_PATCHES_FOLDER_URL/0002-Integrate-the-Mali-GPU-address-to-the-rk3288-and-rk3.patch &&
-wget $KERNEL_PATCHES_FOLDER_URL/0003-Post-Mali-Kernel-device-drivers-modifications.patch
-export PATCHES="0001-Rockchip-DRM-and-Framebuffer-patches-from-ARM-softwa.patch 0002-Integrate-the-Mali-GPU-address-to-the-rk3288-and-rk3.patch 0003-Post-Mali-Kernel-device-drivers-modifications.patch"
-git apply $PATCHES &&
-rm $PATCHES
-unset PATCHES
 
-# Apply a patch to the Mali Midgard driver that adapt the
-# get_user_pages calls to the new signature.
-export MALI_PATCHES_FOLDER=$PATCHES_FOLDER_URL/Mali/$MALI_VERSION
-wget $MALI_PATCHES_FOLDER/0001-Adapt-get_user_pages-calls-to-use-the-new-calling-pr.patch &&
-git apply 0001-Adapt-get_user_pages-calls-to-use-the-new-calling-pr.patch &&
-rm 0001-Adapt-get_user_pages-calls-to-use-the-new-calling-pr.patch
+download_and_apply_patches $KERNEL_PATCHES_FOLDER_URL $KERNEL_PATCHES
+download_and_apply_patches $MALI_PATCHES_FOLDER $MALI_PATCHES
 
 # Get the configuration file and compile the kernel
 export ARCH=arm
-#export CROSS_COMPILE=armv7a-hardfloat-linux-gnueabi-
+export CROSS_COMPILE=armv7a-hardfloat-linux-gnueabi-
 make mrproper
-wget -O .config 'https://raw.githubusercontent.com/Miouyouyou/MyyQi/master/boot/config-4.9.0MyyMyy%2B'
-# make rk3288-miqi.dtb zImage modules -j5
+wget -O .config "$BASE_FILES_URL/$GITHUB_REPO/$GIT_BRANCH/boot/config-4.10.0-rc3RockMyyX-rc+"
+make rk3288-miqi.dtb zImage modules -j5
 
 # Kernel compiled
 # This will just copy the kernel files and libraries in /tmp
@@ -54,4 +82,6 @@ wget -O .config 'https://raw.githubusercontent.com/Miouyouyou/MyyQi/master/boot/
 # mkdir /tmp/MyyQi/boot &&
 # make INSTALL_MOD_PATH=/tmp/MyyQi modules_install &&
 # make INSTALL_PATH=/tmp/MyyQi/boot install
+# cp arch/arm/boot/zImage /tmp/MyyQi/boot
+# cp arch/arm/boot/dts/rk3288-miqi.dtb /tmp/MyyQi/boot
 
