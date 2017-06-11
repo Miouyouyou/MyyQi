d20dad4260340a6c25bc0eec200767a1d2cf4ac9
Myy <myy@miouyouyou.fr>
2017-06-11 21:56:25 +0000

    Trying to fix Tinkerboard's reboot issue this time.
    If that small fix does not work, this will prove to be harder than I
    thought...
    
    Signed-off-by: Myy <myy@miouyouyou.fr>

--- GetPatchAndCompileKernel.sh
+++ GetPatchAndCompileKernel.sh
@@ -59,6 +59,7 @@ export KERNEL_PATCHES="
 0014-ARMbian-RK3288-DTSI-changes.patch
 0015-Enabling-Tinkerboard-s-Wifi-Third-tentative.patch
 0016-Added-support-for-Tinkerboard-s-SPI-interface.patch
+0017-Reboot-hack-for-Tinkerboards.patch
 0100-First-Mali-integration-test-for-ASUS-Tinkerboards.patch
 0200-The-Tinkerboard-DTS-file-maintained-by-TonyMac32-and.patch
 0300-Adding-Mali-Midgard-and-VCodec-support-to-Firefly-RK.patch
